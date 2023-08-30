fb1603a7693154a2b8f334f95eb13c6739de22da
Lenka Zivkovic <lenka.zivkovic@forga.io>
2023-08-30 11:13:54 +0200

    More confetti

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ export default function App() {
   jsConfetti.addConfetti({
    emojis: ['👑', '⚡️', '💥', '✨', '💫', '🌸', '💸'],
   })
+  jsConfetti.addConfetti({
+    confettiColors: [
+      '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+    ],
+  })
   
   jsConfetti.addConfetti()
   return (
